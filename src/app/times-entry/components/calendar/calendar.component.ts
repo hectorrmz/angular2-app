a973b295d51b219bd750ffe3057f6f64b35c9130
Hector Ramirez <hramirez@bofifederalbank.com>
2018-09-06 17:19:46 -0500

    fixed calendar bugs and date issues

--- src/app/times-entry/components/calendar/calendar.component.ts
+++ src/app/times-entry/components/calendar/calendar.component.ts
@@ -8,134 +8,134 @@ import { Week, Day, Time } from '../../../models/Calendar';
 import { TimeEntry, Activity } from '../../../models/Redmine';
 
 @Component({
-    selector: 'app-calendar',
-    styleUrls: ['./calendar.component.scss'],
-    templateUrl: './calendar.component.html',
-    encapsulation: ViewEncapsulation.None
+  selector: 'app-calendar',
+  styleUrls: ['./calendar.component.scss'],
+  templateUrl: './calendar.component.html',
+  encapsulation: ViewEncapsulation.None,
 })
 export class CalendarComponent implements OnChanges {
+  @Input()
+  entries: Array<TimeEntry>;
+  @Input()
+  activities: Array<Activity>;
 
-    @Input() entries: Array<TimeEntry>;
-    @Input() activities: Array<Activity>;
+  weeks: Array<Week> = [];
+  times: Array<Time> = [];
 
-    weeks: Array<Week> = [];
-    times: Array<Time> = [];
+  selected: Time;
 
-    selected: Time;
+  currentView: number;
 
-    currentView: number;
+  constructor(
+    private calendarHelper: CalendarHelper,
+    private redMineService: RedMineService
+  ) {
+    this.setdaysRange();
+    this.currentView = 0;
+  }
 
-    constructor(private calendarHelper: CalendarHelper, private redMineService: RedMineService) {
+  setdaysRange() {
+    let now = new Date();
 
-        this.setdaysRange();
-        this.currentView = 0;
+    let firstDay: number = now.getDate() <= 15 ? 1 : 16;
+    let end: number =
+      now.getDate() <= 15
+        ? 15
+        : this.calendarHelper.daysInMonth(now.getMonth(), now.getFullYear());
+
+    let initial = new Date(`${now.getMonth() + 1}/${firstDay}/${now.getFullYear()}`);
+    let initialNumber: number = initial.getDay(); // 0-6
+
+    if (initialNumber === 6) {
+      //if saturday is Month 1st, move to Sunday to avoid empty week render
+      initial = new Date(
+        `${now.getMonth() + 1}/${firstDay + 1}/${now.getFullYear()}`
+      );
+      initialNumber = 0;
     }
 
-    setdaysRange() {
-        let now = new Date();
+    let dayNumber = initial.getDate(); // date 1
+    let skip = false;
 
-        let firstDay: number = now.getDate() <= 15 ? 1 : 16;
-        let end: number = now.getDate() <= 15 ? 15 : this.calendarHelper.daysInMonth(now.getMonth(), now.getFullYear());
+    while (end > dayNumber) {
+      let week = new Week();
 
-        let initial = new Date(`${now.getMonth() + 1}/${firstDay}/${now.getFullYear()}`);
-
-        let initialNumber: number = initial.getDay(); // 0-6
-        let dayNumber = initial.getDate(); // date 1
-        let skip = false;
-
-        while (end > dayNumber) {
-
-            let week = new Week();
-
-            week.days.forEach((day: Day, index) => {
-
-                if ((index >= initialNumber || skip) && dayNumber <= end) {
-                    day.date = dayNumber;
-                    day.times.date = moment(`${now.getMonth() + 1}-${dayNumber}-${now.getFullYear()}`).format('YYYY-MM-DD');
-                    dayNumber++;
-                }
-            });
-
-            skip = true;
-
-            this.weeks.push(week);
+      week.days.forEach((day: Day, index) => {
+        if ((index >= initialNumber || skip) && dayNumber <= end) {
+          day.date = dayNumber;
+          day.times.date = moment(
+            `${now.getMonth() + 1}-${dayNumber}-${now.getFullYear()}`
+          ).format('YYYY-MM-DD');
+          dayNumber++;
         }
+      });
+
+      skip = true;
+
+      this.weeks.push(week);
     }
 
-    createTimeEntries() {
+    console.log(this.weeks);
+  }
 
-        this.entries.forEach((time: TimeEntry) => {
+  createTimeEntries() {
+    this.entries.forEach((time: TimeEntry) => {
+      let date = moment(time.spent_on).date();
 
-            let date = moment(time.spent_on).date();
+      let entry: Time = {
+        title: time.comments,
+        duration: time.hours,
+        date: date,
+        activity: time.activity,
+      };
 
-            let entry: Time = {
-                title: time.comments,
-                duration: time.hours,
-                date: date,
-                activity: time.activity
-            };
+      this.times.push(entry);
+    });
 
-            this.times.push(entry);
+    this.setLoggedTimeEntries();
+  }
+
+  setLoggedTimeEntries() {
+    this.weeks.forEach((week: Week) => {
+      week.days.forEach((day: Day) => {
+        day.times.entries = this.times.filter(entry => entry.date === day.date);
+      });
+    });
+  }
+
+  getTotal(entries: Array<Time>): number {
+    let total: number = 0;
+
+    for (let i = 0, _len = entries.length; i < _len; i++) {
+      total += entries[i].duration;
+    }
+
+    return total;
+  }
+
+  ngOnChanges(changes) {
+    console.log(changes);
+    this.createTimeEntries();
+  }
+
+  saveTimes() {
+    let newEntries: Time[] = [];
+
+    this.weeks.forEach((week: Week) => {
+      week.days.forEach((day: Day) => {
+        day.times.entries.forEach((time: Time) => {
+          if (time.isNew) {
+            newEntries.push(time);
+          }
         });
+      });
+    });
 
-        this.setLoggedTimeEntries();
-
+    console.log(newEntries);
+    if (confirm('Are you sure you want to submit this times entries?')) {
+      this.redMineService
+        .setTimeEntries(newEntries)
+        .subscribe((response: any) => console.log(response));
     }
-
-    setLoggedTimeEntries() {
-
-        this.weeks.forEach((week: Week) => {
-
-            week.days.forEach((day: Day) => {
-
-                day.times.entries = this.times.filter((entry) =>
-                    entry.date === day.date
-                );
-
-            });
-
-        });
-    }
-
-    getTotal(entries: Array<Time>): number {
-        let total: number = 0;
-
-        for (let i = 0, _len = entries.length; i < _len; i++) {
-            total += entries[i].duration;
-        }
-
-        return total;
-    }
-
-    ngOnChanges(changes) {
-        console.log(changes);
-        this.createTimeEntries();
-    }
-
-    saveTimes() {
-
-        let newEntries: Time[] = [];
-
-        this.weeks.forEach((week: Week) => {
-
-            week.days.forEach((day: Day) => {
-
-                day.times.entries.forEach((time: Time) => {
-                    if (time.isNew) {
-                        newEntries.push(time);
-                    }
-                });
-
-            });
-
-        });
-
-        console.log(newEntries);
-        if (confirm('Are you sure you want to submit this times entries?')) {
-            this.redMineService.setTimeEntries(newEntries)
-                .subscribe((response: any) =>
-                    console.log(response)
-                );
-        }
-    }
+  }
 }
--- src/app/times-entry/times-entry.component.ts
+++ src/app/times-entry/times-entry.component.ts
@@ -10,73 +10,79 @@ import { CalendarHelper } from '../services/calendar-helper.service';
 import { Time } from '../models/Calendar';
 
 @Component({
-    selector: 'times-entry',
-    templateUrl: './times-entry.component.html'
+  selector: 'times-entry',
+  templateUrl: './times-entry.component.html',
 })
 export class TimesEntryComponent {
+  projects: Array<Project>;
+  issues: Array<Issue>;
+  activities: Array<Activity>;
+  entries: Array<Time>;
 
-    projects: Array<Project>;
-    issues: Array<Issue>;
-    activities: Array<Activity>;
-    entries: Array<Time>;
+  issueId: number;
 
-    issueId: number;
+  constructor(
+    private redmineService: RedMineService,
+    private authHelper: AuthHelper,
+    private calendarHelper: CalendarHelper
+  ) {
+    this.issueId = this.authHelper.getIssueId();
 
-    constructor(
-        private redmineService: RedMineService,
-        private authHelper: AuthHelper,
-        private calendarHelper: CalendarHelper
-    ) {
-        this.issueId = this.authHelper.getIssueId();
-
-        if (this.issueId) {
-            this.getTimes(this.issueId);
-        } else {
-            this.getProjects();
-            this.getIssues();
-        }
-
-        this.getActivities();
-
+    if (this.issueId) {
+      this.getTimes(this.issueId);
+    } else {
+      this.getProjects();
+      this.getIssues();
     }
 
-    getProjects() {
-        this.redmineService.getProjects().finally(() => console.log('Projects Called!'))
-            .subscribe((response: any) => this.projects = response.projects);
-    }
+    this.getActivities();
+  }
 
-    getIssues() {
-        this.redmineService.getIssues().finally(() => console.log('Issues Called!'))
-            .subscribe((response: any) => this.issues = response.issues);
-    }
+  getProjects() {
+    this.redmineService
+      .getProjects()
+      .finally(() => console.log('Projects Called!'))
+      .subscribe((response: any) => (this.projects = response.projects));
+  }
 
-    getActivities(){
-        this.redmineService.getActivities().finally(() => console.log('Activities Called!'))
-        .subscribe((response: any) => this.activities = response.time_entry_activities);
-    }
+  getIssues() {
+    this.redmineService
+      .getIssues()
+      .finally(() => console.log('Issues Called!'))
+      .subscribe((response: any) => (this.issues = response.issues));
+  }
 
-    getTimes(issueId: number) {
+  getActivities() {
+    this.redmineService
+      .getActivities()
+      .finally(() => console.log('Activities Called!'))
+      .subscribe(
+        (response: any) => (this.activities = response.time_entry_activities)
+      );
+  }
 
-        let today = new Date();
+  getTimes(issueId: number) {
+    let today = new Date();
 
-        let currentMont = today.getMonth();
-        let currentYear = today.getFullYear();
+    let currentMonth = today.getMonth();
+    let currentYear = today.getFullYear();
 
-        let lastDayInMonth = this.calendarHelper.daysInMonth(currentMont, currentYear);
+    let lastDayInMonth = this.calendarHelper.daysInMonth(currentMonth, currentYear);
 
-        let dateRange = `><${currentYear}-${currentMont + 1}-01|${currentYear}-${currentMont + 1}-${lastDayInMonth}`;
+    let month: any = currentMonth > 9 ? currentMonth + 1 : '0' + (currentMonth + 1);
 
-        this.redmineService.getTimeEntries(issueId, dateRange).finally(() => console.log('Times Called!'))
-            .subscribe((response: any) => this.entries = response.time_entries);
-    }
+    let dateRange = `><${currentYear}-${month}-01|${currentYear}-${month}-${lastDayInMonth}`;
 
-    setIssue(issue: Issue) {
+    this.redmineService
+      .getTimeEntries(issueId, dateRange)
+      .finally(() => console.log('Times Called!'))
+      .subscribe((response: any) => (this.entries = response.time_entries));
+  }
 
-        this.issueId = issue.id;
+  setIssue(issue: Issue) {
+    this.issueId = issue.id;
 
-        this.authHelper.setIssueId(this.issueId);
-        this.getTimes(this.issueId);
-
-    }
-
+    this.authHelper.setIssueId(this.issueId);
+    this.getTimes(this.issueId);
+  }
 }
