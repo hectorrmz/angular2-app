929bfcaeadde130908b21d747a451800eb5dc06b
Hector Ramirez <hector.rmz11@gmail.com>
2018-09-08 19:11:55 -0500

    added outlook component

--- src/app/times-entry/components/outlook/outlook.component.ts
+++ src/app/times-entry/components/outlook/outlook.component.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit } from '@angular/core';
-import { OutlookService } from './outlook.service';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Time } from 'models/Calendar';
+import { OutlookService, AuthHelper } from '../../../services';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'outlook',
@@ -8,30 +10,64 @@ import { ActivatedRoute } from '@angular/router';
   styleUrls: ['./outlook.component.scss'],
 })
 export class OutlookComponent implements OnInit {
+  @Output() outlookEventsImported = new EventEmitter<Time[]>();
   code: string;
+  events: Time[] = [];
+  isAuthorized: boolean = false;
   constructor(
     private outlookService: OutlookService,
-    private route: ActivatedRoute
+    private route: ActivatedRoute,
+    private authHelper: AuthHelper,
+    private location: Location
   ) {}
 
   ngOnInit(): void {
     this.getAuthCode();
+    this.isAuthorized = (this.authHelper.getCode && this.authHelper.getMSToken() && true);
   }
-  getOutlookAuthorization() {
-    this.outlookService.authorizeApp();
+
+  importOutlookCalendar() {
+    this.outlookService.getCalendarView('2018-09-01', '2018-09-30').subscribe(res=>{
+
+        let outlookData = res.value;
+
+        outlookData.forEach(ev => {
+          let date = new Date(ev.Start);
+          this.events.push({
+            title: ev.Subject,
+            date: date.getDate(),
+            duration: this.getEventDuration(ev.Start, ev.End),
+            activity: {
+              id: 10,
+              name: 'Meeting'
+            },
+            isNew: true
+          });
+        });
+
+        this.outlookEventsImported.emit(this.events);
+
+    }, err=>{
+      console.log(err);
+    });
+  }
+
+  private getEventDuration(start: string, end:string): number{
+      let startDate = new Date(start);
+      let endDate = new Date(end);
+
+      return (endDate.getTime() - startDate.getTime()) / 36e5;
   }
 
   private getAuthCode(): void {
     this.route.queryParams.subscribe(params => {
       this.code = params['code'];
       if (this.code) {
-        localStorage.setItem('code', this.code);
-        this.outlookService.getToken().subscribe(
-          res => {
-            if (res.status === 200) {
-              let response = JSON.parse(res._body);
-              localStorage.setItem('access_token', response.access_token);
-            }
+        this.authHelper.setCode(this.code);
+        this.outlookService.getToken().subscribe(res => {
+             this.authHelper.setMSToken(res.access_token);
+             this.location.replaceState('');
+             this.isAuthorized = true;
           },
           err => {
             console.log(err);
