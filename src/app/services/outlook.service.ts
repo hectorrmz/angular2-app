bbcccffdcc41140e0ea1dd7f8b65bdcb8d1884e4
Hector Ramirez <hector.rmz11@gmail.com>
2018-09-08 19:10:56 -0500

    fixed outlook service

--- src/app/services/outlook.service.ts
+++ src/app/services/outlook.service.ts
@@ -1,21 +1,22 @@
 import { Injectable } from '@angular/core';
 import { Http, RequestOptions, Headers } from '@angular/http';
 import { Observable } from 'rxjs/Observable';
+import { AuthHelper } from '../services';
 
 @Injectable()
 export class OutlookService {
   headers: Headers = new Headers();
   options: RequestOptions = new RequestOptions();
 
-  constructor(private http: Http) {
+  constructor(private http: Http, private authHelper: AuthHelper) {
 
-    this.headers.append('Athorization', `Bearer ${window.localStorage.getItem('access_token')}`);
+    this.headers.append('Athorization', `Bearer ${this.authHelper.getMSToken()}`);
     this.options.headers = this.headers;
 
   }
 
   getToken(): any {
-    return this.http.post('token', { code: localStorage.getItem('code') })
+    return this.http.post('token', { code: this.authHelper.getCode() })
           .map(res => res.json())
           .catch((error: any) => Observable.throw(error.json()));
   }
